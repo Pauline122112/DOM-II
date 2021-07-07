2c38c5b8989dd98868b0917d5fb97ab9117b8afb
Pauline122112 <pauline.design007@gmail.com>
2021-07-06 22:15:01 -0500

    enlarged H1 and also gave the h1 background color upon mouseover

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 // Your code goes here
-const mouseNav = document.querySelectorAll('a')
-console.log(mouseNav)
+const mouseOver = document.querySelector('.nav, a')
+console.log(mouseOver)
+
+const headOne = document.querySelector('.logo-heading')
+console.log(headOne)
 
 
-document.addEventListener('mouseover', function( event ) {
+mouseOver.addEventListener('mouseover', function( event ) {
    event.target.style.color = 'orange'
    setTimeout(function() {
        event.target.style.color = ''
@@ -12,3 +15,19 @@ document.addEventListener('mouseover', function( event ) {
 }, false)
 
 
+//1 Mouseover <h1> yellow up
+
+// console.log(headONE);
+    headOne.addEventListener('mouseover', e => {
+        e.target.style.backgroundColor = 'yellow'; 
+    });
+    headOne.addEventListener('mouseleave', e => {
+        e.target.style.backgroundColor = 'white';   
+    });
+
+    headOne.addEventListener('mouseover', e => {
+        e.target.style.fontSize = '80px'
+        setTimeout(function() {
+            e.target.style.fontSize = ''
+        }, 500)
+    }, false)
