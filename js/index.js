a09c94ad4c3a227a8043537a0672b46f2302c222
Pauline122112 <pauline.design007@gmail.com>
2021-07-06 21:27:10 -0500

    added mouseover event listener but still having issues

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // Your code goes here
-const mouseNav = document.querySelectorAll('.nav')
+const mouseNav = document.querySelectorAll('a')
+console.log(mouseNav)
+
+
+document.addEventListener('mouseover', function( event ) {
+   event.target.style.color = 'orange'
+   setTimeout(function() {
+       event.target.style.color = ''
+   }, 500)
+
+}, false)
+
+
